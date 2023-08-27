31bab168e5378bfcbe8ecc9a72c7bf1a6e17218a
Sebastian Iglesias <sebastian@getcaribou.com>
2023-08-27 14:42:12 -0300

    remove install script

--- js/main.js
+++ js/main.js
@@ -7,17 +7,6 @@ window.addEventListener('beforeinstallprompt', (e) => {
   deferredPrompt = e;
 });
 
-const installApp = document.getElementById('installApp');
-installApp.addEventListener('click', async () => {
-  if (deferredPrompt !== null) {
-    deferredPrompt.prompt();
-    const { outcome } = await deferredPrompt.userChoice;
-    if (outcome === 'accepted') {
-      deferredPrompt = null;
-    }
-  }
-});
-
 const players = []
 
 function addPlayer(name) {
